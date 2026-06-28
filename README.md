d5d78818ac2e1ce3281d0c0b8310bdac1a161b90
54145a <133004095+54145a@users.noreply.github.com>
2026-06-28 09:46:09 +0800

    update deps

--- README.md
+++ README.md
@@ -2,22 +2,22 @@
 
 ## Usage
 ```typescript
-import { querySelector } from "@keupoz/strict-queryselector";
+import { querySelector } from '@keupoz/strict-queryselector'
 
 // Same examples applies to querySelectorAll
 // Gets body element with type of HTMLBodyElement
-const body = querySelector("body", HTMLBodyElement);
+const body = querySelector('body', HTMLBodyElement)
 
 // Throws TypeError as body element is not instance of HTMLDivElement
-const bodyDiv = querySelector("body", HTMLDivElement);
+const bodyDiv = querySelector('body', HTMLDivElement)
 
 // Throws Error if element doesn't exist
 // If it exists, checks if it's instance of specified type
-const meaningOfLife = querySelector(".life-meaning", HTMLDivElement);
+const meaningOfLife = querySelector('.life-meaning', HTMLDivElement)
 ```
 
 ### Wrappers
 There are wrapper functions that allows to wrap the type guard around custom element. And they return the same function as regular `querySelector`.
 ```typescript
-const bodyQuerySelector = wrapQuerySelector(document.body);
+const bodyQuerySelector = wrapQuerySelector(document.body)
 ```
--- index.ts
+++ index.ts
@@ -1,49 +1,49 @@
-export type ElementInterface<T extends Element> = {
-    prototype: T;
-    new(): T;
-};
+export interface ElementInterface<T extends Element> {
+  prototype: T
+  new(): T
+}
 
 /** Assert element is not null */
 export function assertElementFound(selector: string, element: Element | null): asserts element is Element {
-    if (element === null) {
-        throw new Error(`Element "${selector}" is not found`);
-    }
+  if (element === null) {
+    throw new Error(`Element "${selector}" is not found`)
+  }
 }
 
 /** Assert element is instance of provided interface */
 export function assertTypeOfElement<T extends Element>(selector: string, element: Element, type: ElementInterface<T>): asserts element is T {
-    if (!(element instanceof type)) {
-        throw new TypeError(`Element "${selector}" is not ${type.name}`);
-    }
+  if (!(element instanceof type)) {
+    throw new TypeError(`Element "${selector}" is not ${type.name}`)
+  }
 }
 
 /** Wrap the type guard around parent's querySelector method */
 export function wrapQuerySelector(parent: ParentNode) {
-    return <T extends Element>(selector: string, type: ElementInterface<T>): T => {
-        const element = parent.querySelector(selector);
+  return <T extends Element>(selector: string, type: ElementInterface<T>): T => {
+    const element = parent.querySelector(selector)
 
-        assertElementFound(selector, element);
-        assertTypeOfElement(selector, element, type);
+    assertElementFound(selector, element)
+    assertTypeOfElement(selector, element, type)
 
-        return element;
-    };
+    return element
+  }
 }
 
 /** Wrap the type guard around parent's querySelectorAll method */
 export function wrapQuerySelectorAll(parent: ParentNode) {
-    return <T extends Element>(selector: string, type: ElementInterface<T>): NodeListOf<T> => {
-        const elements = parent.querySelectorAll<T>(selector);
+  return <T extends Element>(selector: string, type: ElementInterface<T>): NodeListOf<T> => {
+    const elements = parent.querySelectorAll<T>(selector)
 
-        for (let i = 0; i < elements.length; i++) {
-            assertTypeOfElement(`(${selector})[${i}]`, elements[i], type);
-        }
+    for (let i = 0; i < elements.length; i++) {
+      assertTypeOfElement(`(${selector})[${i}]`, elements[i], type)
+    }
 
-        return elements;
-    };
+    return elements
+  }
 }
 
 /** The type guard wrapped around document's querySelector method */
-export const querySelector = wrapQuerySelector(document);
+export const querySelector = wrapQuerySelector(document)
 
 /** The type guard wrapped around document's querySelectorAll method */
-export const querySelectorAll = wrapQuerySelectorAll(document);
+export const querySelectorAll = wrapQuerySelectorAll(document)
